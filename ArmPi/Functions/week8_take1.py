222d248cde8a3c2500edc178c7da44887f7a5990
bestelh <bestelh@oregonstate.edu>
2024-03-02 13:29:25 -0800

    take 1

--- ArmPi/Functions/week8_take1.py
+++ ArmPi/Functions/week8_take1.py
@@ -125,28 +125,16 @@ class Perception:
 
         return img
 
-    def main_loop(self):
-        while True:
-            img = self.my_camera.frame
-            if img is not None:
-                frame = img.copy()
-                Frame = self.run(frame)
-                cv2.imshow('Frame', Frame)
-                key = cv2.waitKey(1)
-                if key == 27:
-                    break
-        self.my_camera.camera_close()
-        cv2.destroyAllWindows()
-
 
 class Move:
-    def __init__(self, location, color):
+    def __init__(self):
+        # Initialize your class attributes here
         self.rect = None
         self.track = None
         self._stop = None
-        self.get_roi = None
-        self.unreachable = None
-        self.__isRunning = None
+        self.get_roi = None  # Add get_roi attribute
+        self.unreachable = None  # Add unreachable attribute
+        self.__isRunning = None  # Add __isRunning attribute
         self.detect_color = None
         self.action_finish = None
         self.rotation_angle = None
@@ -158,120 +146,96 @@ class Move:
         self.count = None
         self.start_pick_up = None
         self.first_move = None
+        
+    def process_perception_data(self, perception):
+        self.detect_color = perception.__target_color
+        self.locations = perception.locations
 
-        # 不同颜色木快放置坐标(x, y, z)
-        self.coordinate = {
-            'red': (-15 + 0.5, 12 - 0.5, 1.5),
-            'green': (-15 + 0.5, 6 - 0.5, 1.5),
-            'blue': (-15 + 0.5, 0 - 0.5, 1.5),
-        }
-
-        self.location = location
-        self.color = color
-
-    def set_rgb(self, color):
-        # Implementation of set_rgb method
-
-    def set_buzzer(self, duration):
-        # Implementation of set_buzzer method
-
-    def get_angle(self, x, y, angle):
-        # Implementation of get_angle method
-
-    def init_move(self):
-        # Implementation of init_move method
-
-    def perform_move(self):
+    def move_block(self, location, color):
+        coordinate = {
+            'red':   (-15 + 0.5, 12 - 0.5, 1.5),
+            'green': (-15 + 0.5, 6 - 0.5,  1.5),
+            'blue':  (-15 + 0.5, 0 - 0.5,  1.5),
+            }
+        
         while True:
-            if self.__isRunning:
-                if self.first_move and self.start_pick_up:  # 当首次检测到物体时
-                    self.action_finish = False
-                    self.set_rgb(self.detect_color)
-                    self.set_buzzer(0.1)
-                    result = AK.setPitchRangeMoving((self.world_X, self.world_Y - 2, 5), -90, -90, 0)
+            if __isRunning:
+                if first_move and start_pick_up:  
+                    action_finish = False
+                    set_rgb(detect_color)
+                    setBuzzer(0.1)
+                    result = AK.setPitchRangeMoving((world_X, world_Y - 2, 5), -90, -90, 0)
                     if result == False:
-                        self.unreachable = True
+                        unreachable = True
                     else:
-                        self.unreachable = False
-                    time.sleep(result[2] / 1000)
-                    self.start_pick_up = False
-                    self.first_move = False
-                    self.action_finish = True
-                elif not self.first_move and not self.unreachable:  # 不是第一次检测到物体
-                    self.set_rgb(self.detect_color)
-                    if self.track:  # 如果是跟踪阶段
-                        if not self.__isRunning:  # 停止以及退出标志位检测
+                        unreachable = False
+                    time.sleep(result[2]/1000)  # 返回参数的第三项为时间
+                    start_pick_up = False
+                    first_move = False
+                    action_finish = True
+                elif not first_move and not unreachable:  # 不是第一次检测到物体
+                    set_rgb(detect_color)
+                    if track:  # 如果是跟踪阶段
+                        if not __isRunning:  # 停止以及退出标志位检测
                             continue
-                        AK.setPitchRangeMoving((self.world_x, self.world_y - 2, 5), -90, -90, 0, 20)
+                        AK.setPitchRangeMoving((world_x, world_y - 2, 5), -90, -90, 0, 20)
                         time.sleep(0.02)
-                        self.track = False
-                    if self.start_pick_up:  # 如果物体没有移动一段时间，开始夹取
-                        self.action_finish = False
-                        if not self.__isRunning:  # 停止以及退出标志位检测
+                        track = False
+                    if start_pick_up:  # 如果物体没有移动一段时间，开始夹取
+                        action_finish = False
+                        if not __isRunning:  # 停止以及退出标志位检测
                             continue
-                        Board.setBusServoPulse(1, self.servo1 - 280, 500)  # 爪子张开
-                        servo2_angle = self.get_angle(self.world_X, self.world_Y, self.rotation_angle)
+                        Board.setBusServoPulse(1, servo1 - 280, 500)  # 爪子张开
+                        # 计算夹持器需要旋转的角度
+                        servo2_angle = getAngle(world_X, world_Y, rotation_angle)
                         Board.setBusServoPulse(2, servo2_angle, 500)
                         time.sleep(0.8)
-                        if not self.__isRunning:
+
+                        if not __isRunning:
                             continue
-                        AK.setPitchRangeMoving((self.world_X, self.world_Y, 2), -90, -90, 0, 1000)  # 降低高度
+                        AK.setPitchRangeMoving((world_X, world_Y, 2), -90, -90, 0, 1000)  # 降低高度
                         time.sleep(2)
-                        if not self.__isRunning:
-                            continue
-                        Board.setBusServoPulse(1, self.servo1, 500)  # 夹持器闭合
-                        time.sleep(1)
-                        if not self.__isRunning:
-                            continue
-                        Board.setBusServoPulse(2, 500, 500)
-                        AK.setPitchRangeMoving((self.world_X, self.world_Y, 12), -90, -90, 0, 1000)  # 机械臂抬起
-                        time.sleep(1)
-                        if not self.__isRunning:
-                            continue
-                        result = AK.setPitchRangeMoving((self.coordinate[self.detect_color][0],
-                                                        self.coordinate[self.detect_color][1], 12), -90, -90, 0)
-                        time.sleep(result[2] / 1000)
-                        if not self.__isRunning:
-                            continue
-                        servo2_angle = self.get_angle(self.coordinate[self.detect_color][0],
-                                                     self.coordinate[self.detect_color][1], -90)
-                        Board.setBusServoPulse(2, servo2_angle, 500)
-                        time.sleep(0.5)
-                        if not self.__isRunning:
-                            continue
-                        AK.setPitchRangeMoving((self.coordinate[self.detect_color][0],
-                                                self.coordinate[self.detect_color][1],
-                                                self.coordinate[self.detect_color][2] + 3), -90, -90, 0, 500)
-                        time.sleep(0.5)
-                        if not self.__isRunning:
-                            continue
-                        result = AK.setPitchRangeMoving((self.coordinate[self.detect_color]), -90, -90, 0, 1000)
-                        time.sleep(0.8)
-                        if not self.__isRunning:
-                            continue
-                        Board.setBusServoPulse(1, self.servo1 - 200, 500)  # 爪子张开，放下物体
-                        time.sleep(0.8)
-                        if not self.__isRunning:
-                            continue
-                        result = AK.setPitchRangeMoving((self.coordinate[self.detect_color][0],
-                                                        self.coordinate[self.detect_color][1], 12), -90, -90, 0, 800)
-                        time.sleep(0.8)
-                        self.init_move()  # 回到初始位置
+
+                        # ... (continue with the rest of your code)
+
+                        initMove()  # 回到初始位置
                         time.sleep(1.5)
-                        self.detect_color = 'None'
-                        self.first_move = True
-                        self.get_roi = False
-                        self.action_finish = True
-                        self.start_pick_up = False
-                        self.set_rgb(self.detect_color)
+
+                        detect_color = 'None'
+                        first_move = True
+                        get_roi = False
+                        action_finish = True
+                        start_pick_up = False
+                        set_rgb(detect_color)
                     else:
                         time.sleep(0.01)
             else:
-                if self._stop:
-                    self._stop = False
-                    Board.setBusServoPulse(1, self.servo1 - 70, 300)
+                if _stop:
+                    _stop = False
+                    Board.setBusServoPulse(1, servo1 - 70, 300)
                     time.sleep(0.5)
                     Board.setBusServoPulse(2, 500, 500)
                     AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
                     time.sleep(1.5)
                 time.sleep(0.01)
+
+def main_loop(perception, move):
+    while True:
+        img = perception.my_camera.frame
+        if img is not None:
+            frame = img.copy()
+            Frame = perception.run(frame)
+            move.process_perception_data(perception)
+            # Add code here to use the color and location data in the Move object
+            cv2.imshow('Frame', Frame)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+    perception.my_camera.camera_close()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    perception = Perception()
+    move = Move()
+    perception.start()
+    main_loop(perception, move)
