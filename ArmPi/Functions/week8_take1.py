9644a340dd326fded2522b34aef35ff256fb069b
bestelh <bestelh@oregonstate.edu>
2024-03-02 14:52:54 -0800

    add re initilazation

--- ArmPi/Functions/week8_take1.py
+++ ArmPi/Functions/week8_take1.py
@@ -12,6 +12,7 @@ from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
+
 Gripper_servo = 1
 Gripper_twist=2
 Wrist=3
@@ -23,5 +24,10 @@ Shoulder_twist=6
 servo_angle=500
 
 AK = ArmIK()
+
+def initMove():
+    Board.setBusServoPulse(1, servo_angle - 50, 300)
+    Board.setBusServoPulse(2, 500, 500)
+    AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
 
 Board.setBusServoPulse(Gripper_servo, servo_angle - 280, 500)
