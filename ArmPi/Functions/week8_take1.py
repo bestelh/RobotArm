b71012f146321fdac4bdb474d316c0e359551b7d
bestelh <bestelh@oregonstate.edu>
2024-03-02 15:31:48 -0800

    starting out

--- ArmPi/Functions/week8_take1.py
+++ ArmPi/Functions/week8_take1.py
@@ -24,10 +24,32 @@ Shoulder_twist=6
 servo_angle=500
 
 AK = ArmIK()
+class Movement: 
 
-def initMove():
-    Board.setBusServoPulse(1, servo_angle - 50, 300)
-    Board.setBusServoPulse(2, 500, 500)
-    AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
+    def _init_(self):
+        self.coordinate = {
+            'red':   (-15 + 0.5, 12 - 0.5, 1.5),
+            'green': (-15 + 0.5, 6 - 0.5,  1.5),
+            'blue':  (-15 + 0.5, 0 - 0.5,  1.5),
+        }
+        self.angle = 0
 
-Board.setBusServoPulse(Gripper_servo, servo_angle - 280, 500)
+    def initMove(self):
+        Board.setBusServoPulse(1, servo_angle - 50, 300)
+        Board.setBusServoPulse(2, 500, 500)
+        self.AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
+
+    def openGripper(self):
+        Board.setBusServoPulse(1, servo_angle - 280, 500)
+        time.sleep(1)
+
+    def closeGripper(self):
+        Board.setBusServoPulse(1, servo_angle, 500)
+
+
+
+if __name__ == "__main__":
+    move=Movement()
+    move.openGripper()
+    time.sleep(2)
+    move.closeGripper()
